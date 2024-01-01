164f09880e21ec33172493416277166d0bcf3146
Danimal <danimal@hackpoetic.com>
2024-01-01 14:53:33 -0500

    feat: populate basic landing page

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -1,7 +1,17 @@
+// Ours - Styles
+import styles from "./Landing.module.css";
+
 export default function Landing() {
   return (
     <main>
-      <h1>Landing</h1>
+      <section className={styles.hero}>
+        <h1>Simple Visual Timer</h1>
+        <p>
+          Our purposeis to provide a clutter-free timer that embraces minimalism
+          in order to not torture neurdivergent brains like ours.
+        </p>
+        <button>Start Timer</button>
+      </section>
     </main>
   );
 }
